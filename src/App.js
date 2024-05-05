c6ce2667230ca668833ec654a93f7bd34cb72c1d
Deividson Henry <deividson.henrique2016@hotmail.com>
2024-05-05 17:09:06 -0300

    day 05.05.24

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Banner from "./components/Banner";
 import Card from "./components/Card";
+import Category from "./components/Category";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/db.json";
+import videos from "./json/videos.json";
+
+const categories = [
+"Geografia",
+"Como fazer e usar",
+"Astronomia e Geografia",
+"Climatologia, Meteorologia, Vegetação",
+"Geologia e Hidrografia"]
+
+function filterCategory(id){
+  return videos.filter( video => video.category === categories[id])
+}
 
 function App() {
   return (
@@ -11,10 +23,27 @@ function App() {
     <Header/>
     <Banner image="favoritos"/>
     <Container>
-    <h2>Geografia</h2>
-      <section className="cards">
-        {videos.map(video => <Card id={video.id} key={video.id} />)}
-      </section>
+
+      <Category category={categories[0]}>
+        {filterCategory(0).map(video => <Card id={video.id} key={video.id} />)}
+      </Category>
+
+      <Category category={categories[1]}>
+        {filterCategory(1).map(video => <Card id={video.id} key={video.id} />)}
+      </Category>
+
+      <Category category={categories[2]}>
+        {filterCategory(2).map(video => <Card id={video.id} key={video.id} />)}
+      </Category>
+      
+      <Category category={categories[3]}>
+        {filterCategory(3).map(video => <Card id={video.id} key={video.id} />)}
+      </Category>
+
+      <Category category={categories[4]}>
+        {filterCategory(4).map(video => <Card id={video.id} key={video.id} />)}
+      </Category>
+
     </Container>
     <Footer/>
     </>
