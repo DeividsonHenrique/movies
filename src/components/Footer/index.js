3d6ac7a759e957600bfda9d1772a54c6b54bdc98
Deividson Henry <deividson.henrique2016@hotmail.com>
2024-05-04 21:46:17 -0300

    day 04.05.24

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import styles from "./Footer.module.css"
+
+function Footer(){
+    return(
+        <>
+        <footer className={styles.footer}>
+            <h2>MaiaFlix &copy; Desenvolvido por Henry</h2>
+        </footer>
+        </>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import styles from "./Header.module.css"
+
+function Header(){
+    return(
+        <>
+        <header className={styles.header}>
+           <span>MaiaFlix</span> 
+           <nav>
+            <a href="#">Home</a>
+            <a href="#">Assistir</a>
+           </nav>
+        </header>
+        </>
+    )
+}
+
+export default Header;
