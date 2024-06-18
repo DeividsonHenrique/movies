6506702adf2ad49111af689c56ef63e7b1ce62d3
Bowie <deividson.henrique2016@hotmail.com>
2024-06-18 17:13:35 -0300

    reactimages

--- src/Pages/Favorites/index.js
+++ src/Pages/Favorites/index.js
@@ -1,34 +1,46 @@
-import styles from "./Favorites.module.css"
-import Header from "../../components/Header"
-import Container from "../../components/Container"
-import Footer from "../../components/Footer"
-import VideoList from "../../components/VideoList"
-import { useFavoriteContext } from "../../contexts/Favorites"
+import styles from "./Favorites.module.css";
+import Header from "../../components/Header";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+import VideoList from "../../components/VideoList";
+import { useFavoriteContext } from "../../contexts/Favorites";
 import ScrollToTopButton from "../../components/ScrollToTopButton";
+import ReactImageMagnify from "react-image-magnify";
 
+function Favorites() {
+  const { favorite } = useFavoriteContext();
 
-function Favorites(){
+  return (
+    <>
+      <ScrollToTopButton />
+      <Header />
+      <Container>
+        <section className={styles.favorites}>
+          <h2>Meus Favoritos</h2>
+          {<VideoList videos={favorite} emptyHeading={"Sem Favoritos"} />}
+        </section>
 
-    const { favorite } = useFavoriteContext()
-
-    return(
-        <>
-        <ScrollToTopButton />
-            <Header/>
-            <Container>
-                <section className={styles.favorites}>
-                    <h2>Meus Favoritos</h2>
-                    { <VideoList videos={favorite} emptyHeading={"Sem Favoritos"} /> }
-                    </section>
-
-                    <div className={styles.empty}>
-                        <img src="https://images.pexels.com/photos/7947304/pexels-photo-7947304.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="empty" />
-                        
-                    </div>
-            </Container>
-            <Footer/>
-        </>
-    )
+        <div className={styles.empty}>
+          <ReactImageMagnify
+            {...{
+              smallImage: {
+                alt: "Wristwatch by Ted Baker London",
+                isFluidWidth: true,
+                src: "https://images.pexels.com/photos/7947304/pexels-photo-7947304.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+              },
+              largeImage: {
+                src: "https://images.pexels.com/photos/7947304/pexels-photo-7947304.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                width: 500,
+                height: 800,
+              },
+            }}
+          />
+          
+        </div>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
-export default Favorites
+export default Favorites;
